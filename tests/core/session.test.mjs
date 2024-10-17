5eda8ea2125c7b2bff35189ef5457dd2e086cf9b
mdos-san <mdossan@protonmail.com>
2024-10-17 02:12:26 +0200

    core: Session entity

---
+++ tests/core/session.test.mjs
@@ -0,0 +1,15 @@
+import assert from "node:assert";
+import test from "node:test";
+import { Session } from "../../src/core/session.mjs";
+import { AbstractEntity } from "../../src/core/AbstractEntity.mjs";
+
+test("Can create a session", async () => {
+  const session = new Session("sessionId", "userId");
+  assert.ok(session);
+  assert.ok(session instanceof AbstractEntity);
+  assert.strictEqual(session.getCollection(), "session");
+  assert.deepStrictEqual(session.getDataAsJSON(), {
+    id: "sessionId",
+    userId: "userId",
+  });
+});
