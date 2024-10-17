7081d828579af7dfdff0d71b6787957a8ac1373d
mdos-san <mdossan@protonmail.com>
2024-10-17 02:12:26 +0200

    core: User entity
    TODO

---
+++ tests/core/user.test.mjs
@@ -0,0 +1,20 @@
+import assert from "node:assert";
+import test from "node:test";
+import { User } from "../../src/core/user.mjs";
+import { AbstractEntity } from "../../src/core/AbstractEntity.mjs";
+
+test("Can create a user and check password", async () => {
+  const id = "userId1";
+  const password = "MySuperSecretPassword";
+  const user = await User.create(id, password);
+  assert.strictEqual(user.id, id);
+  assert.notStrictEqual(user.hashedPassword, password);
+  assert.strictEqual(await user.checkPassword("WrongPassword"), false);
+  assert.strictEqual(await user.checkPassword(password), true);
+  assert.ok(user instanceof AbstractEntity);
+  assert.strictEqual(user.getCollection(), "user");
+  assert.deepStrictEqual(user.getDataAsJSON(), {
+    id: "userId1",
+    hashedPassword: user.hashedPassword,
+  });
+});
