fba9628829a8405d354739b9631aebad4cc5916c
mdos-san <mdossan@protonmail.com>
2024-10-18 07:33:51 +0200

    api: Route GET /session
    TODO

---
+++ src/session/setupGetSessionApi.mjs
@@ -0,0 +1,34 @@
+import assert from "node:assert";
+import { FindOneRequest } from "../database/FindOneRequest.mjs";
+import { AbstractDatabase } from "../database/AbstractDatabase.mjs";
+
+export function setupGetSessionApi(app, absctractDatabase) {
+  assert.ok(absctractDatabase instanceof AbstractDatabase);
+
+  app.get("/session", async (req, res) => {
+    try {
+      const body = req.body;
+      assert.ok(typeof body === "object", "`body` should be an object");
+      assert.ok(body !== null, "`body` should not be null");
+
+      const { sessionId } = body;
+      assert.ok(
+        typeof sessionId === "string",
+        "`sessionId` should be a string",
+      );
+
+      const findOneRequest = new FindOneRequest("session", sessionId);
+      const sessionDbResponse = await absctractDatabase.findOne(findOneRequest);
+      assert.ok(sessionDbResponse, "session not found");
+
+      res.statusCode = 200;
+      res.send(sessionDbResponse);
+    } catch (e) {
+      res.statusCode = 400;
+      res.statusMessage = e.message;
+      console.error(e);
+    } finally {
+      res.end();
+    }
+  });
+}
