90c5f83d502c3805408727c06fee417a699310b3
mdos-san <mdossan@protonmail.com>
2024-10-20 05:22:41 +0200

    core: Session entity

---
+++ src/core/session.mjs
@@ -0,0 +1,25 @@
+import assert from "node:assert";
+import { AbstractEntity } from "./AbstractEntity.mjs";
+
+export class Session extends AbstractEntity {
+  static collection = "session";
+
+  constructor(id, userId) {
+    super();
+    assert.ok(typeof id === "string", "`id` should be a string");
+    assert.ok(typeof userId === "string", "`userId` should be a string");
+    this.id = id;
+    this.userId = userId;
+  }
+
+  getCollection() {
+    return Session.collection;
+  }
+
+  getDataAsJSON() {
+    return {
+      id: this.id,
+      userId: this.userId,
+    };
+  }
+}
